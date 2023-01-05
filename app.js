2585caaef7c7a647dbd8273348b0d299f18c7787
Eva Marko <valentova.eva92@gmail.com>
2023-01-05 13:04:06 +1300

    Event listeners changes

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ $(".btn2").on("click", () => {
   $(".btn1").text("Sumbit too");
 });
 
+//Get
+let hodnota = $("#color").val();
+
 $(".btn3").on("click", () => {
-  $(".btn1").css("background", "purple");
-  $(".btn2").css("background", "salmon");
-  $(".btn3").css("background", "pink");
+  $(".btn1").css("background", hodnota);
+  $(".btn2").css("background", hodnota);
+  $(".btn3").css("background", hodnota);
 });
